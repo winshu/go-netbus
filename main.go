d60e279f7687378bb17c8577811868d48b4eb337
Kevin Hu <hushuang0529@163.com>
2019-11-18 23:17:18 +0800

    完善配置信息

--- main.go
+++ main.go
@@ -22,14 +22,8 @@ func printWelcome(args []string) {
 }
 
 func printHelp() {
-	fmt.Println(`usage: "-listen port1 port2" example: "nb -listen 1997 2017" `)
-	fmt.Println(`       "-tran port1 ip:port2" example: "nb -tran 1997 192.168.1.2:3389" `)
-	fmt.Println(`       "-slave ip1:port1 ip2:port2" example: "nb -slave 127.0.0.1:3389 8.8.8.8:1997" `)
-	fmt.Println(`============================================================`)
-	fmt.Println(`optional argument: "-log logpath" . example: "nb -listen 1997 2017 -log d:/nb" `)
-	fmt.Println(`log filename format: Y_m_d_H_i_s-agrs1-args2-args3.log`)
-	fmt.Println(`============================================================`)
-	fmt.Println(`if you want more help, please read "README.md". `)
+	fmt.Println(`usage: "-server" start as server`)
+	fmt.Println(`       "-client" start as client`)
 }
 
 func main() {
