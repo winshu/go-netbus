90734e27274f649985ebeecbcad665c4b5edbbf1
hushuang <hushuang@yunjiglobal.com>
2019-11-18 15:06:15 +0800

    完善代码

--- test/netbus_test.go
+++ test/netbus_test.go
@@ -6,22 +6,43 @@ import (
 	"testing"
 )
 
+// -----------------------------------------------------------------------------
+
+// netbus
 func TestServer(t *testing.T) {
-	//config := core.ServerConfig{
-	//	Port:       6666,
-	//	RandomPort: true,
-	//}
-	//core.SingleServer(config)
-	core.Port2Port("6666", "8456")
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	config := core.ServerConfig{
+		Port:       6666,
+		RandomPort: true,
+	}
+	core.SingleServer(config)
 }
 
 func TestClient(t *testing.T) {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
-	//config := core.ClientConfig{
-	//	ServerAddr: "127.0.0.1:6666",
-	//	LocalAddr:  "127.0.0.1:7456",
-	//}
-	//core.Client(config)
+	config := core.ClientConfig{
+		ServerAddr: "127.0.0.1:6666",
+		LocalAddr:  "127.0.0.1:7456",
+	}
+	core.Client(config)
+}
+
+// -----------------------------------------------------------------------------
+
+// server
+func TestPort2Port(t *testing.T) {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	core.Port2Port("6666", "8456")
+}
+
+// client
+func TestHost2Host(t *testing.T) {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	core.Host2Host("127.0.0.1:7456", "127.0.0.1:6666")
+}
+
+func TestPort2Host(t *testing.T) {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	core.Port2Host("6666", "127.0.0.1:7456")
 }
