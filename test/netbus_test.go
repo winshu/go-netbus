c9f2687a73abd39138b432537012de868d6394dc
hushuang <hushuang@yunjiglobal.com>
2019-11-20 13:14:27 +0800

    微调单元测试

--- test/netbus_test.go
+++ test/netbus_test.go
@@ -24,8 +24,10 @@ func TestServer(t *testing.T) {
 
 func TestClient(t *testing.T) {
 	cfg := config.ClientConfig{
-		ServerAddr:     config.NetAddress{IP: "10.3.8.119", Port: 6666},
-		LocalAddr:      []config.NetAddress{{"127.0.0.1", 7456}},
+		ServerAddr: config.NetAddress{IP: "10.3.8.119", Port: 6666},
+		LocalAddr: []config.NetAddress{
+			{"127.0.0.1", 7456},
+		},
 		MaxRedialTimes: 10,
 	}
 	core.Client(cfg)
